3567173e966acdd5e8c6e8004497dc09f624b1bd
hugo <hfernandez@neuquen.gov.ar>
2017-10-02 16:57:44 -0300

    primer commit

---
+++ activator.ts
@@ -0,0 +1,233 @@
+    // My Imports
+    import * as configPrivate from './config.private';
+    import * as codeVerification from './utils/codigoVerificacion';
+    import {
+        sendMail
+    } from './utils/sendMail';
+    import {
+        sendSms
+    } from './utils/sendSms';
+    import {
+        Matching
+    } from '@andes/match';
+
+
+    // Imports 3rd Parties
+    let counter = 0;
+    let expirationOffset = 1000 * 60 * 60 * 24 * 365;
+    let mongoClient = require('mongodb').MongoClient;
+    let winston = require('winston');
+    let cantidadMatch = 0;
+    let contador = 0;
+    let urlAndes = configPrivate.urlMongoAndes;
+    let urlMpi = configPrivate.urlMongoMpi;
+    let coleccionPacientesApp = 'pacienteApp';
+    let coleccionPacientes = 'paciente';
+    let logger = new(winston.Logger)({
+        transports: [
+            new(winston.transports.Console)(),
+            new(winston.transports.File)({
+                filename: 'activador.log'
+            })
+        ]
+    });
+
+    let condicion = {
+        createdAt: {
+            $gte: new Date("2017-09-26T00:00:00.000Z")
+        }
+    };
+
+    mongoClient.connect(urlMpi, function (err, db) {
+        mongoClient.connect(urlAndes, function (err, db2) {
+
+            db.collection(coleccionPacientes).find(condicion).toArray(function (error, pacientes: any) {
+                if (error) {
+                    console.log('Error al conectarse a la base de datos ', error);
+                }
+                if (pacientes.length > 0) {
+                    
+                    pacientes.forEach(async pac => {
+
+                        let celular = searchContacto(pac, 'celular');
+                        if (celular) {
+                            let pacienteVinculado = await buscaPacienteVinculado(pac, db2);
+                            
+                            if (pacienteVinculado) {
+
+                                let matcheo = await matchPacientes(pac, pacienteVinculado);
+                                if (matcheo >= 0.95) {
+
+                                    if (pacienteVinculado.pacientes.length <= 0) {
+
+                                        logger.log('info', 'Envío sms a : ', pac.documento, pac.nombre, pac.apellido);
+
+                                        // let objAdd = {
+                                        //     id: pac._id,
+                                        //     relacion: 'principal',
+                                        //     addedAt: new Date()
+
+                                        // }
+                                        // pacienteVinculado.pacientes.push(objAdd);
+                                        // db2.collection(coleccionPacientesApp).updateOne({
+                                        //     _id: pacienteVinculado._id
+                                        // }, {
+                                        //     $set: {
+                                        //         pacientes: pacienteVinculado.pacientes
+                                        //     }
+                                        // }, function (err, rta) {
+                                        //     if (err) {
+                                        //         console.log('Error: ', err);
+                                        //     } else {
+                                        //         logger.log('info', 'Paciente vinculado: ', pacienteVinculado);
+                                        //         enviarCodigoVerificacion(pacienteVinculado);
+                                        //     }
+
+                                        // })
+
+
+                                    }
+                                }
+                            } else {
+                                contador++;
+
+                                // codeVerification.createUniqueCode(db2, coleccionPacientesApp)
+                                //     .then(codigo => {
+                                //         // Creamos el pacienteApp y lo insertamos con un codigo de verificación
+                                //         let dataPacienteApp: any = {
+                                //             nombre: pac.nombre,
+                                //             apellido: pac.apellido,
+                                //             email: null,
+                                //             password: null,
+                                //             telefono: celular,
+                                //             envioCodigoCount: 0,
+                                //             nacionalidad: 'Argentina',
+                                //             documento: pac.documento,
+                                //             fechaNacimiento: pac.fechaNacimiento,
+                                //             sexo: pac.sexo,
+                                //             genero: pac.genero,
+                                //             codigoVerificacion: codigo,
+                                //             expirationTime: new Date(Date.now() + expirationOffset),
+                                //             permisos: [],
+                                //             pacientes: [{
+                                //                 id: pac._id,
+                                //                 relacion: 'principal',
+                                //                 addedAt: new Date()
+                                //             }]
+                                //         };
+                                //         db2.collection(coleccionPacientesApp).insertOne({dataPacienteApp}, function(err , user){
+                                //             if (err) {
+                                //                 console.log('error', err);
+                                //             } else {
+                                //                 enviarCodigoVerificacion(user);
+                                //             }
+
+                                //         });
+                                //     });
+                            }
+
+
+                        } else {
+
+                            logger.log('info', 'PACIENTE SIN CELULAR!!: ', pac.nombre, pac.apellido, pac.documento);
+
+                        }
+                        counter++
+                        if (counter === pacientes.length) {
+                            db.close();
+                            console.log('Cantidad de pacientes que matchearon: ', cantidadMatch);
+                            console.log('Para que Silvi sea feliz: ', contador);
+                            console.log('Proceso terminado');
+                        }
+                    });
+
+                } else {
+                    console.log('No existen pacientes con esa condición');
+                }
+            });
+        });
+    });
+
+    function searchContacto(pacienteData, key) {
+        if (pacienteData.contacto) {
+            for (let i = 0; i < pacienteData.contacto.length; i++) {
+                if (pacienteData.contacto[i].tipo === key) {
+                    return pacienteData.contacto[i].valor;
+                }
+            }
+        }
+        return null;
+    }
+
+    function buscaPacienteVinculado(unPaciente: any, db2) {
+        return new Promise((resolve, reject) => {
+
+            db2.collection(coleccionPacientesApp).findOne({
+                'documento': unPaciente.documento
+            }, function (err, item) {
+                if (err) {
+                    console.log('entro por este error:', err);
+                    reject(err);
+
+                } else {
+                    if (item) {
+                        resolve(item);
+                    } else {
+                        resolve(null);
+                    }
+
+                }
+
+            });
+
+        });
+    };
+
+    function matchPacientes(pacienteApp, pacienteVinculado) {
+        return new Promise((resolve, reject) => {
+            let match = new Matching();
+            let pacApp = {
+                apellido: pacienteApp.apellido,
+                nombre: pacienteApp.nombre,
+                sexo: pacienteApp.sexo.toUpperCase(),
+                fechaNacimiento: pacienteApp.fechaNacimiento,
+                documento: pacienteApp.documento
+            };
+            let pac = {
+                apellido: pacienteVinculado.apellido,
+                nombre: pacienteVinculado.nombre,
+                sexo: pacienteVinculado.sexo.toUpperCase(),
+                fechaNacimiento: pacienteVinculado.fechaNacimiento,
+                documento: pacienteVinculado.documento
+            }
+            let valorMatching = match.matchPersonas(pacApp, pac, configPrivate.mpi.weightsDefault, 'Levenshtein');
+            if (valorMatching) {
+                resolve(valorMatching)
+            } else {
+                resolve(null)
+            }
+        })
+    };
+
+    function enviarCodigoVerificacion(user) {
+
+        let mailOptions: any = {
+            from: configPrivate.enviarMail.host,
+            to: user.email,
+            subject: 'Ministerio de Salud :: ANDES :: Código de activación',
+            text: 'Estimado ' + user.email + ', Su código de activación para ANDES Mobile es: ' + user.codigoVerificacion,
+            html: 'Estimado ' + user.email + ', Su código de activación para ANDES Mobile es: ' + user.codigoVerificacion,
+        };
+
+        let smsOptions: any = {
+            telefono: user.telefono,
+            mensaje: user.codigoVerificacion
+        };
+
+        sendMail(mailOptions);
+        sendSms(smsOptions, function (res) {
+            if (res === '0') {
+                logger.log('info', 'El SMS se envío correctamente');
+            }
+        });
+    }
---
+++ utils/codigoVerificacion.ts
@@ -0,0 +1,39 @@
+
+
+export function listadoCodigos(db:any, col:any) {
+   return db.collection(col).find({ codigoVerificacion: { $ne: null } }, {codigoVerificacion: 1, _id: 0}).then(listado => {
+       let numeros = listado.map((item: any) => item.codigoVerificacion);
+       return Promise.resolve(numeros);
+   }).catch(() => Promise.reject([]));
+}
+
+/**
+* Genera un código de verificación.
+* @param onlyNumber
+*/
+export function generarCodigoVerificacion(onlyNumber = true) {
+   let codigo = '';
+   let length = 6;
+   let caracteres = onlyNumber ? '0123456789' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   for (let i = 0; i < length; i++) {
+       codigo += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+   }
+
+   return codigo;
+}
+
+/**
+* Genera un codigo unico chequeando con la db de pacientes.
+*/
+
+export function createUniqueCode(db, col) {
+   return listadoCodigos(db,col).then((listado) => {
+       let codigo = generarCodigoVerificacion();
+       while (listado.indexOf(codigo) >= 0) {
+           codigo = generarCodigoVerificacion();
+       }
+
+       return Promise.resolve(codigo);
+
+   });
+}
